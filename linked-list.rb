1514b9de1bd95e7dd22ad26456e94e5bdc932007
Shua Miller <thejoshmill@gmail.com>
2021-01-05 12:32:29 -0700

    Add code to methods.

--- linked-list.rb
+++ linked-list.rb
@@ -4,39 +4,56 @@ class LinkedList
   end
 
   def append(value)
-    Node.new()
+    @array.push Node.new(value)
   end
 
   def prepend(value)
-
+    @array.unshift Node.new(value)
   end
 
   def size
-
+    @array.length
   end
 
   def head
-
+    @array.first
   end
 
   def tail
-
+    @array.last
   end
 
   def at(index)
-
+    @array.fetch(index)
   end
 
   def pop
-
+    @array.pop
   end
 
   def contains?(value)
+    @array.each { |node| 
+      if node.value = value
+        true
+      end
+    }
+    return false
+  end
 
+  def find(value)
+    @array.each { |node|
+      if node.value = value
+        @array.index(node)
+      end
+    }
+    return nil
   end
   
   def to_s
-
+    @array.each {|node|
+      print "( #{node.value} ) -> "
+    }
+    print nil
   end
 end
 

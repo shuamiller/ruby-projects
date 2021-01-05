df4ed46d4e9f099451d4ef53c85280da00d2924b
Shua Miller <thejoshmill@gmail.com>
2021-01-05 11:42:24 -0700

    Create LinkedList class and Node class.

---
+++ linked-list.rb
@@ -0,0 +1,19 @@
+class LinkedList
+  def initialize
+    array = []
+  end
+
+  def append(value)
+    
+  end
+end
+
+class Node
+  @value = nil
+  @next_node = nil
+
+  def initialize(value, next_node)
+    @value = value
+    @next_node = next_node
+  end
+end
